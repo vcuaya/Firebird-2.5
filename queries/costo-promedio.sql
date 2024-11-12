c3a9574be04c1438b37f74cb8c40375dc55f7382
Víctor Cuaya <vcuaya@outlook.com>
2024-11-12 15:28:13 -0600

    feat: add query to retrieve average cost

---
+++ queries/costo-promedio.sql
@@ -0,0 +1,18 @@
+/*
+ Regresa el cálculo del costo promedio de los productos considerando la
+ cantidad y el costo de los movimientos, cuyo documento no está cancelado y
+ donde el almacén es: 1, 12 o 16.
+ */
+SELECT cve_art,
+    SUM(cant * costo) / SUM(cant) AS costo_promedio
+FROM minve01
+    LEFT JOIN compc01 ON minve01.refer = compc01.cve_doc
+    LEFT JOIN compr01 ON minve01.refer = compr01.cve_doc
+WHERE minve01.tipo_doc IN ('c', 'r')
+    AND minve01.almacen IN (1, 12, 16)
+    AND (
+        compc01.status <> 'C'
+        OR compr01.status <> 'C'
+    )
+GROUP BY cve_art
+ORDER BY cve_art
