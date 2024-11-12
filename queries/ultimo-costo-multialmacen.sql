98a43978adacc830b699d29182537cb688246a65
Víctor Cuaya <vcuaya@outlook.com>
2024-11-12 15:35:26 -0600

    feat: add query to retrieve the last cost
    - add query to retrieve the last cost of a product from multiple warehouses

---
+++ queries/ultimo-costo-multialmacen.sql
@@ -0,0 +1,24 @@
+/*
+ Regresa el costo de los productos, tomando en cuenta el último costo, pero
+ solo si el documento no está cancelado y el almacén es: 1, 5, 12, 14 o 16.
+ */
+SELECT minve01.cve_art,
+    costo,
+    num_mov
+FROM minve01
+    INNER JOIN(
+        SELECT cve_art,
+            MAX(minve01.num_mov) mov
+        FROM minve01
+            LEFT JOIN compc01 ON minve01.refer = compc01.cve_doc
+            LEFT JOIN compr01 ON minve01.refer = compr01.cve_doc
+        WHERE minve01.tipo_doc IN ('c', 'r')
+            AND minve01.almacen IN (1, 5, 12, 14, 16)
+            AND (
+                compc01.status <> 'C'
+                OR compr01.status <> 'C'
+            )
+        GROUP BY cve_art
+    ) ultimo on ultimo.cve_art = minve01.cve_art
+    AND ultimo.mov = minve01.num_mov
+ORDER BY minve01.cve_art
